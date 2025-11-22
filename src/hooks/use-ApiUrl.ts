195800b83632fbe2a533c8abbcb9816f65aa5b73
mamun <mamun166036@yahoo.com>
2025-11-22 11:21:45 +0600

    up

--- src/hooks/use-ApiUrl.ts
+++ src/hooks/use-ApiUrl.ts
@@ -6,11 +6,11 @@ export default function useApiUrl() {
   const DevUrl = "http://localhost:5104/api";
   const DevRootUrl = "http://localhost:5104";
 
-  // let ProductionUrl = "http://119.148.62.103:6503/api";
-  // let ProductionRootUrl = "http://119.148.62.103:6503";
+  let ProductionUrl = "http://119.148.62.103:6503/api";
+  let ProductionRootUrl = "http://119.148.62.103:6503";
 
-  let ProductionUrl = "http://103.95.98.115:6307/api";
-  let ProductionRootUrl = "http://103.95.98.115:6307";
+  // let ProductionUrl = "http://103.95.98.115:6307/api";
+  // let ProductionRootUrl = "http://103.95.98.115:6307";
 
 
   // let ProductionUrl = "http://api-nextappbd.runasp.net/api";
