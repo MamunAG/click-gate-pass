ad23942284ecb3a8d84458190643752bb7d7bda7
mamun <mamun166036@yahoo.com>
2025-12-01 14:33:17 +0600

    up

--- src/components/module-cards.tsx
+++ src/components/module-cards.tsx
@@ -1,126 +1,80 @@
 
 import {
     Card,
-    CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import { BriefcaseBusiness, ChartGantt, Container, Factory, HandCoins, Settings, ShieldCheck, SquareChartGantt, SwatchBook } from "lucide-react"
+import { BadgeDollarSign, BriefcaseBusiness, ChartGantt, Container, Factory, Forklift, HandCoins, Settings, ShieldCheck, Ship, SquareActivity, SquareChartGantt, Stamp, SwatchBook } from "lucide-react"
+import { ReactNode } from "react";
+
+function ModuleCard({ children }: { children: ReactNode | ReactNode[]; }) {
+    return (
+        <Card className="hover:cursor-pointer p-5">
+            <CardTitle className="text-xl font-semibold tabular-nums @[250px]/card:text-2xl flex items-center gap-3">
+                {children}
+            </CardTitle>
+        </Card>
+    )
+}
 
 export function ModuleCards() {
     return (
         <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-3 @5xl/main:grid-cols-5">
-            <Card className="hover:cursor-pointer">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <Settings />
-                        <span>Master Settings</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <BriefcaseBusiness />
-                        <span>Merchandising</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <ChartGantt />
-                        <span>Planning</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <SquareChartGantt />
-                        <span>Procurement</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <HandCoins />
-                        <span>Payroll</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <Factory />
-                        <span>GMT Production</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <Container />
-                        <span>Inventory</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <SwatchBook />
-                        <span>Textile</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <ShieldCheck />
-                        <span>IE</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <ShieldCheck />
-                        <span>Quality</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <ShieldCheck />
-                        <span>Commercial</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <ShieldCheck />
-                        <span>Accounting</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <ShieldCheck />
-                        <span>Logistic</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <Card className="">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl flex items-center gap-3">
-                        <ShieldCheck />
-                        <span>Print $ Embroidery</span>
-                    </CardTitle>
-                </CardHeader>
-            </Card>
+            <ModuleCard>
+                <Settings />
+                <span>Master Settings</span>
+            </ModuleCard>
+            <ModuleCard>
+                <BriefcaseBusiness />
+                <span>Merchandising</span>
+            </ModuleCard>
+            <ModuleCard>
+                <ChartGantt />
+                <span>Planning</span>
+            </ModuleCard>
+            <ModuleCard>
+                <SquareChartGantt />
+                <span>Procurement</span>
+            </ModuleCard>
+            <ModuleCard>
+                <HandCoins />
+                <span>Payroll</span>
+            </ModuleCard>
+            <ModuleCard>
+                <Factory />
+                <span>GMT Production</span>
+            </ModuleCard>
+            <ModuleCard>
+                <Container />
+                <span>Inventory</span>
+            </ModuleCard>
+            <ModuleCard>
+                <SwatchBook />
+                <span>Textile</span>
+            </ModuleCard>
+            <ModuleCard>
+                <SquareActivity />
+                <span>IE</span>
+            </ModuleCard>
+            <ModuleCard>
+                <ShieldCheck />
+                <span>Quality</span>
+            </ModuleCard>
+            <ModuleCard>
+                <Ship />
+                <span>Commercial</span>
+            </ModuleCard>
+            <ModuleCard>
+                <BadgeDollarSign />
+                <span>Accounting</span>
+            </ModuleCard>
+            <ModuleCard>
+                <Forklift />
+                <span>Logistic</span>
+            </ModuleCard>
+            <ModuleCard>
+                <Stamp />
+                <span>Print $ Embroidery</span>
+            </ModuleCard>
         </div>
     )
 }
